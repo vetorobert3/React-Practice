51c35291c1822fcf9fdae477a2267120c7ce9de0
Veto Ramirez <vetorobert3@gmail.com>
2020-06-29 19:37:53 -0700

    Finished up about page. App Done. Might add some more functionality later

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 import Navbar from './components/layout/Navbar';
 import Joke from './components/Joke';
+import About from './components/Pages/About';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Navbar />
         <div className='container mx-auto'>
           <Route path="/" exact component={Joke} />
+          <Route path="/about" exact component={About} />
         </div> 
       </div>
     </Router>
--- src/components/Pages/About.js
+++ src/components/Pages/About.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 export default function About() {
   return (
-    <div>
-      
+    <div className='mx-auto'>
+      <h1>This is a React JS App created by Veto Ramirez</h1>
+      <p>This tutorial comvers React Hooks, Functional Components, and React Router</p>
     </div>
   )
 }
